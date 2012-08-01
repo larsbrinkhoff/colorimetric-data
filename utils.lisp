083573871ecfe021829732a4a63bc72a062aed40
Lars Brinkhoff <lars@nocrew.org>
2012-08-01 10:27:37 +0200

    Fix for defconstant redefinition.

--- utils.lisp
+++ utils.lisp
@@ -4,8 +4,7 @@
   `(eval-when (:execute :compile-toplevel :load-toplevel)
      (declaim (type (simple-array ,type (,(length value))) ,name))
      (defconstant ,name
-       ,(make-array
-	 (length value)
-	 :element-type type
-	 :initial-contents (loop for x in value collect (coerce x type))))))
-
+       (if (constantp ',name)
+	   ,name
+	   ,(make-array (length value) :element-type type :initial-contents
+			(loop for x in value collect (coerce x type)))))))
